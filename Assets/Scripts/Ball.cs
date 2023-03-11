f0e1e225f764078dc16cdb98bddf6201cde436bd
Shu Matsumoto <shumatsumoto0328@gmail.com>
2023-03-11 13:24:42 +0900

    クリックしてstart

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,18 +5,24 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     public bool isDead = false;
+    public float speed = 3.5f;
+    public float accelSpeed = 2.1f;
+    bool isStart = false;
     Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.AddForce(new Vector3(150, -150, 0));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isStart == false && Input.GetMouseButtonDown(0))
+        {
+            isStart = true;
+            rb.AddForce(new Vector3(1, -1, 0) * speed, ForceMode.VelocityChange);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -33,9 +39,10 @@ public class Ball : MonoBehaviour
 
         if (collision.gameObject.name == "Bar")
         {
+            speed += accelSpeed;
             Vector3 vec = transform.position - collision.transform.position;
             rb.velocity = Vector3.zero;
-            rb.AddForce(vec.normalized * 150);
+            rb.AddForce(vec.normalized * speed, ForceMode.VelocityChange);
         }
     }
 }
